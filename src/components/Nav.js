bf29695841b926afb21ffb1b2044cdff052498eb
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-08-24 01:31:07 -0400

    adding links and updating lodash.template

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,10 +12,11 @@ export default class Nav extends Component {
               . . .
             </button>
             <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-              <a className="dropdown-item" href="#">About Me</a>
-              <a className="dropdown-item" href="#">Portfolio</a>
+              <a className="dropdown-item" href="#/aboutme">About Me</a>
+              <a className="dropdown-item" href="#/portfolio">Portfolio</a>
               <a className="dropdown-item" href="https://github.com/nicolehopkins">Github</a>
               <a className="dropdown-item" href="https://nicjs.home.blog/">Blog</a>
+              <a className="dropdown-item" href="#/contact">Contact Me</a>
             </div>
           </div>
         </div>
