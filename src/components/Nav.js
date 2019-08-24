a5eb7a4c29ee1041ab33333d9bcbd870772b67ff
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-08-24 13:17:26 -0400

    adding more styling to home page and _blank to outside links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,10 +12,10 @@ export default class Nav extends Component {
               . . .
             </button>
             <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-              <a className="dropdown-item" href="#/aboutme">About Me</a>
-              <a className="dropdown-item" href="#/portfolio">Portfolio</a>
-              <a className="dropdown-item" href="https://github.com/nicolehopkins">Github</a>
-              <a className="dropdown-item" href="https://nicjs.home.blog/">Blog</a>
+              <a className="dropdown-item" href='#/aboutme'>About Me</a>
+              <a className="dropdown-item" href='#/portfolio'>Portfolio</a>
+              <a className="dropdown-item" href='https://github.com/nicolehopkins' target='_blank' rel='noopener noreferrer'>Github</a>
+              <a className="dropdown-item" href='https://nicjs.home.blog/' target='_blank' rel='noopener noreferrer'>Blog</a>
               <a className="dropdown-item" href="#/contact">Contact Me</a>
             </div>
           </div>
